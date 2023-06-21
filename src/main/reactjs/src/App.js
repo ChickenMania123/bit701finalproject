73daf6ef57d81670fd01039f068def8a4eb5dab9
kyunghyun <rudgus6370@gmail.com>
2023-06-21 16:24:55 +0900

    first commit

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ju from './ju.png';
+
+function App() {
+  return (
+    <div className="App">
+      <img alt='' src={ju} width={300} border="1"/>
+      <br/><br/>
+      <h2>Docker 배포 성공 기원!!!!</h2>
+    </div>
+  );
+}
+
+export default App;
