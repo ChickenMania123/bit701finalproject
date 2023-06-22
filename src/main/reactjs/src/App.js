e43b6fc41d8f1aaa985212df33d15da7f92f6318
kyunghyun <rudgus6370@gmail.com>
2023-06-22 16:14:08 +0900

    dockerfile 수정2

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <img alt='' src={ju} width={300} border="1"/>
       <br/><br/>
-      <h2>Docker 배포 성공 기원!!!!</h2>
+      <h2>Docker 배포 성공 가능!!!!</h2>
     </div>
   );
 }
